9d42a3328208f013ce6dd55df6765bffc57cfc43
shun989 <minhtuanvt89@gmail.com>
2021-04-18 22:42:56 +0700

    up file

--- taoDTduyNhat.php
+++ taoDTduyNhat.php
@@ -1,1 +1,19 @@
 <?php
+class Application
+{
+    private static $instance;
+
+    private function __construct(){ }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Application();
+            echo 'hello';
+        }
+        return self::$instance;
+    }
+}
+$app1 = Application::getInstance();
+$app2 = Application::getInstance();
+//$app3 = new Application();//se bao loi.
