c216875d7083fcc9a69d81979651b54233242bea
shun989 <minhtuanvt89@gmail.com>
2021-04-18 22:30:57 +0700

    up file

--- demDoiTuongCua1Lop.php
+++ demDoiTuongCua1Lop.php
@@ -1,1 +1,25 @@
 <?php
+class Application{
+    private $name;
+    public static $count = 0;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        self::$count++;
+    }
+
+    public function __toString()
+    {
+        return 'Application: ' . $this->name;
+    }
+
+}
+
+echo 'Total objects count: ' . Application::$count. '<br>';
+$app1 = new Application('App one');
+echo 'Total objects count: ' . Application::$count. '<br>';
+$app2 = new Application('App two');
+echo 'Total objects count: ' . Application::$count. '<br>';
+echo $app1 . '<br>';
+echo $app2;
